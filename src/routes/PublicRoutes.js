66cf83694e24abf77d0a0b238b42a02ee6932e6a
Nicole <nicoleotieno18@gmail.com>
2020-01-13 14:12:25 +0300

    include public routes

--- src/routes/PublicRoutes.js
+++ src/routes/PublicRoutes.js
@@ -4,6 +4,10 @@ import Navbar from '../Components/Navbar';
 import Home from '../Components/Home';
 import About from '../Components/About';
 import Contact from '../Components/Contact';
+import Dashboard from '../Components/Dashboard';
+import Farmers from '../Components/Farmers';
+import Buyers from '../Components/Buyers';
+import FarmExp from '../Components/FarmExp';
 import SignUp from '../Components/SignUp';
 import Login from '../Components/Login';
 
@@ -15,6 +19,10 @@ const PublicRoutes = ({ match }) => (
       <Route path='/about' component={About} />
       <Route path='/contact' component={Contact} />
       <Route path='/signup' component={SignUp} />
+      <Route path='/dashboard' component={Dashboard} />
+      <Route path='/farmers' component={Farmers} />
+      <Route path='/buyers' component={Buyers} />
+      <Route path='/farmexp' component={FarmExp} />
       <Route path='/login' component={Login} />
     </Switch>
   </div>
