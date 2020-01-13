4188749a217f5314dda3caca3353e708f63b90ca
Nicole <nicoleotieno18@gmail.com>
2020-01-13 10:25:46 +0300

    handle events for login page

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,50 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import history from '../util/history';
 
-class Login extends React.Component {
-  render() {
-    return (
-      <div>
-        <div className='split left'>
-          <div className='center'></div>
-        </div>
-        <div className='split right'>
-          <div className='center'>
-            <form>
-              <div className='form-group'>
-                <input
-                  type='email'
-                  className='form-control'
-                  id='exampleInputPassword1'
-                  placeholder='Email'
-                />
-              </div>
-              <div className='form-group'>
-                <input
-                  type='password'
-                  className='form-control'
-                  id='exampleInputPassword1'
-                  placeholder='Password'
-                />
-              </div>
-              <div className='form-group'>
-                <input
-                  type='password confirmation'
-                  className='form-control'
-                  id='exampleInputPassword1'
-                  placeholder='Password confirmation'
-                />
-              </div>
-              <div className='btn btncenter'>
-                <button type='submit' className='btn btn-primary'>
-                  Login
-                </button>
-              </div>
-            </form>
+function Login() {
+  const [inputs, setInputs] = useState();
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setInputs({ ...inputs, [name]: value });
+  };
+
+  const handleSubmit = async () => {
+    const loginResponse = await axios.post('/login', inputs);
+    console.log(loginResponse.data);
+    // const role = loginResponse.data.role;
+    // localStorage.setItem('roles', JSON.stringify(role));
+    // history.push('/app');
+  };
+  return (
+    <div>
+      <div className='split leftlogin'>
+        <div className='center'></div>
+      </div>
+      <div className='split right'>
+        <div className='center'>
+          <div className='form-group'>
+            <input
+              type='email'
+              name='email'
+              className='form-control'
+              id='exampleInputPassword1'
+              placeholder='Email'
+              onChange={handleChange}
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='password'
+              className='form-control'
+              name='password'
+              id='exampleInputPassword1'
+              placeholder='Password'
+              onChange={handleChange}
+            />
+          </div>
+          <div className='btn btncenter'>
+            <button
+              onClick={handleSubmit}
+              type='submit'
+              className='btn btn-primary'
+            >
+              Login
+            </button>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Login;
