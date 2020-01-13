a95f104a67eef795fb1db94af61531880c5b90be
Nicole <nicoleotieno18@gmail.com>
2020-01-13 10:22:01 +0300

    add a text area within the farming expert's card

--- src/Components/FarmExp.js
+++ src/Components/FarmExp.js
@@ -3,14 +3,25 @@ import React from 'react';
 class FarmExp extends React.Component {
   render() {
     return (
-      <div className='card'>
-        <div className='card-header'>Featured</div>
-        <div className='card-body'>
-          <h5 className='card-title'>Special title treatment</h5>
-          <p className='card-text'>
-            With supporting text below as a natural lead-in to additional
-            content.
-          </p>
+      <div className='f-card'>
+        <div className='card'>
+          <div className='card-body'>
+            <h5 className='card-title'>Special title treatment</h5>
+            <p className='card-text'>
+              With supporting text below as a natural lead-in to additional
+              content.
+            </p>
+            <hr />
+            <textarea
+              name=''
+              id=''
+              cols='30'
+              rows='10'
+              className='txt-area'
+              placeholder='comment...'
+            ></textarea>
+            <button className='f-btn'>send</button>
+          </div>
         </div>
       </div>
     );
