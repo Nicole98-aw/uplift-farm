5b3781ebeb12c0f17696283cbeac54bfa3624f8a
Nicole <nicoleotieno18@gmail.com>
2020-01-14 10:58:15 +0300

    modify App file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React from 'react';
-import { Router, Route, Switch, Redirect } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 import PublicRoutes from './routes/PublicRoutes';
 import PrivateRoutes from './routes/PrivateRoutes';
 import history from './util/history';
 
-const authentication = () =>
-  JSON.parse(localStorage.getItem('roles')) ? (
-    <Redirect to='/app' />
-  ) : (
-    <PublicRoutes />
-  );
+// const authentication = () =>
+//   JSON.parse(localStorage.getItem('roles')) ? (
+//     <Redirect to='/app' />
+//   ) : (
+//     <PublicRoutes />
+//   );
 
 const App = () => {
   return (
     <div>
       <Router history={history}>
         <Switch>
+          <Route path='/' component={PublicRoutes} />
           <Route path='/app' component={PrivateRoutes} />
-          <Route path='' render={authentication} />
+          {/* <Route path='' render={authentication} /> */}
         </Switch>
       </Router>
     </div>
