cc166ca7b1fa6962fc1f509607a27f7876d50c2f
Nicole <nicoleotieno18@gmail.com>
2020-01-14 17:13:22 +0300

    modify user/routers

--- server/routers/users.js
+++ server/routers/users.js
@@ -3,9 +3,22 @@ const Users = require('../models/users');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 
+const { welcome, refresh } = require('../jwt/handler');
+
+// setting up jsonwebtoken
+const jwt = require('jsonwebtoken');
+
+const jwtSecretKey = 'my_secret_key';
+const jwtExpirySeconds = 300;
+
+// const users = {
+//   user1: 'password1',
+//   user2: 'password2'
+// };
+
 router.post('/register', (req, res) => {
-  const { fullNames, email, password, password2 } = req.body;
-  if (!fullNames || !email || !password || !password2) {
+  const { fullNames, email, password, password2, role } = req.body;
+  if (!fullNames || !email || !password || !password2 || !role) {
     res.send({ err: 'Please fill all fields!' });
   } else if (password !== password2) {
     res.send({ err: 'Passwords do not match!' });
@@ -20,7 +33,8 @@ router.post('/register', (req, res) => {
           const newUser = new Users({
             fullNames,
             email,
-            password
+            password,
+            role
           });
           bcrypt.genSalt(10, (err, salt) => {
             bcrypt.hash(password, salt, (err, hash) => {
@@ -40,6 +54,13 @@ router.post('/register', (req, res) => {
 
 router.post('/login', (req, res) => {
   const { email, password } = req.body;
+
+  // validate for emptiness in the req.body
+  if (!email && !password) {
+    // return 401 error is username or password doesn't exist, or if password does
+    // not match the password in our records
+    return res.status(401).end();
+  }
   // Match user
   Users.findOne({
     email: email
@@ -51,12 +72,31 @@ router.post('/login', (req, res) => {
     bcrypt.compare(password, user.password, (err, isMatch) => {
       if (err) throw err;
       if (isMatch) {
-        res.send(user);
+        // generate a token using the user email and user id
+        console.log(user);
+        const { email, _id } = user;
+        const token = jwt.sign({ email, _id }, jwtSecretKey, {
+          algorithm: 'HS256',
+          expiresIn: jwtExpirySeconds
+        });
+        console.log('token:', token);
+
+        // set the cookie as the token string, with a similar max age as the token
+        // here, the max age is in milliseconds, so we multiply by 1000
+        res
+          .cookie('token', token, { maxAge: jwtExpirySeconds * 1000 })
+          .send(token);
+        // res.end();
       } else {
         res.send({ err: 'Password incorrect' });
       }
     });
   });
+});
+
+router.post(`/homepage`, welcome, refresh, (req, res) => {
+  console.log(req.payload);
+  res.send('Successfully visited our homepage');
 });
 
 module.exports = router;
