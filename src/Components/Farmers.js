42948e714b4ef9357476b64d43405cbddb0dd00e
Nicole <nicoleotieno18@gmail.com>
2020-01-16 14:29:36 +0300

    add header

--- src/Components/Farmers.js
+++ src/Components/Farmers.js
@@ -1,119 +1,130 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Col, Row, Button, FormGroup, Label, Input } from 'reactstrap';
 
-class Farmers extends React.Component {
-  render() {
-    return (
-      <div>
-        <h5>Create posts</h5>
-        <Row form>
-          <Col md={4}>
-            <FormGroup>
-              <Label for='exampleEmail'>Product</Label>
-              <Input type='email' name='email' />
-            </FormGroup>
-          </Col>
-          <Col md={4}>
-            <FormGroup>
-              <Label for='examplePassword'>Brief description</Label>
-              <Input type='password' name='password' />
-            </FormGroup>
-          </Col>
-        </Row>
-        <Row form>
-          <Col md={4}>
-            <FormGroup>
-              <Label for='exampleEmail'>Location</Label>
-              <Input type='email' name='email' id='exampleEmail' />
-            </FormGroup>
-          </Col>
-          <Col md={4}>
-            <FormGroup>
-              <Label for='examplePassword'>Name</Label>
-              <Input type='password' name='password' id='examplePassword' />
-            </FormGroup>
-          </Col>
-        </Row>
-        <Row form>
-          <Col md={4}>
-            <FormGroup>
-              <Label for='exampleEmail'>Contact</Label>
-              <Input type='email' name='email' id='exampleEmail' />
-            </FormGroup>
-          </Col>
-        </Row>
-        <Button>Send post</Button>
-        <hr />
-        <h5>Send questions to farming experts</h5>
-        <Row form>
-          <Col md={4}>
-            <FormGroup>
-              <Label for='exampleEmail'>Question</Label>
-              <Input type='email' name='email' id='exampleEmail' />
-            </FormGroup>
-          </Col>
-        </Row>
-        <Button>Send</Button>
-        <hr />
-        <h5>Recent posts from buyers</h5>
-        <div className='card'>
-          <div className='card-body'>
-            <form>
-              <div className='form-group'>
-                <label htmlFor=''>Product</label>
-                <input
-                  type='name'
-                  className='form-control'
-                  id='exampleInputEmail1'
-                  aria-describedby='emailHelp'
-                />
-              </div>
-              <div className='form-group'>
-                <label htmlFor=''>Quantity</label>
-                <input
-                  type='name'
-                  className='form-control'
-                  id='exampleInputEmail1'
-                  aria-describedby='emailHelp'
-                />
-              </div>
-              <div className='form-group'>
-                <label htmlFor=''>Brief descrition</label>
-                <input
-                  type='email'
-                  className='form-control'
-                  id='exampleInputPassword1'
-                />
-              </div>
-              <div className='form-group'>
-                <label htmlFor=''>Location</label>
-                <input
-                  type='password'
-                  className='form-control'
-                  id='exampleInputPassword1'
-                />
-              </div>
-              <div className='form-group'>
-                <label htmlFor=''>Name</label>
-                <input
-                  type='town'
-                  className='form-control'
-                  id='exampleInputPassword1'
-                />
-              </div>
-              <div className='form-group'>
-                <label htmlFor=''>Contact</label>
-                <input
-                  type='town'
-                  className='form-control'
-                  id='exampleInputPassword1'
-                />
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
+const Farmers = () => {
+  const [productStore, setProductStore] = useState([]);
+
+  const [product, setProduct] = useState({
+    productName: '',
+    description: '',
+    location: '',
+    name: '',
+    contact: ''
+  });
+
+  const handleChange = event => {
+    event.preventDefault();
+    const { name, value } = event.target;
+    setProduct({
+      ...product,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    setProductStore([...productStore, { ...product }]);
+    console.log(productStore);
+  };
+
+  const { productName, description, location, name, contact } = product;
+  return (
+    <div>
+      <h5 className='fp'>Create posts</h5>
+      <h5 className='fp-page'>Farmers page</h5>
+      <Row form>
+        <Col md={4}>
+          <FormGroup>
+            <Label>Product Name</Label>
+            <Input
+              type='text'
+              name='productName'
+              value={productName}
+              onChange={handleChange}
+            />
+          </FormGroup>
+        </Col>
+        <Col md={4}>
+          <FormGroup>
+            <Label>Brief description</Label>
+            <Input
+              type='text'
+              name='description'
+              value={description}
+              onChange={handleChange}
+            />
+          </FormGroup>
+        </Col>
+      </Row>
+      <Row form>
+        <Col md={4}>
+          <FormGroup>
+            <Label>Location</Label>
+            <Input
+              type='text'
+              name='location'
+              value={location}
+              onChange={handleChange}
+            />
+          </FormGroup>
+        </Col>
+        <Col md={4}>
+          <FormGroup>
+            <Label>Name</Label>
+            <Input
+              type='text'
+              name='name'
+              value={name}
+              onChange={handleChange}
+            />
+          </FormGroup>
+        </Col>
+      </Row>
+      <Row form>
+        <Col md={4}>
+          <FormGroup>
+            <Label>Contact</Label>
+            <Input
+              type='number'
+              name='contact'
+              value={contact}
+              onChange={handleChange}
+            />
+          </FormGroup>
+        </Col>
+      </Row>
+      <Button
+        className='btn-btn'
+        color='success'
+        style={{ color: 'white' }}
+        onClick={handleSubmit}
+      >
+        Send post
+      </Button>{' '}
+      <hr />
+      <h5>Send questions to farming experts</h5>
+      {/* <Row form>
+        <Col md={4}>
+          <FormGroup>
+            <Label for='exampleName'>Question</Label>
+            <Input type='name' name='name' id='exampleName' />
+          </FormGroup>
+        </Col>
+      </Row> */}
+      <textarea
+        name=''
+        id=''
+        cols='30'
+        rows='10'
+        className='txt-area'
+        placeholder='write something...'
+      ></textarea>
+      <Button className='btn-btn' color='success' style={{ color: 'white' }}>
+        Send
+      </Button>{' '}
+      <hr />
+    </div>
+  );
+};
+
 export default Farmers;
