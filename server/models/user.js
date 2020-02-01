c9f63cff897b38627e22c5dc4d55a5b912462a7b
Nicole <nicoleotieno18@gmail.com>
2020-02-01 18:04:45 +0300

    create user schema

---
+++ server/models/user.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  fullNames: {
+    required: true,
+    type: String
+  },
+  role: {
+    type: Array,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model('User', userSchema);
