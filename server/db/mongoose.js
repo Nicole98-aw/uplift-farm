bc1b34486142cf271a07929048f662ed66c9c286
Nicole <nicoleotieno18@gmail.com>
2020-02-01 18:00:18 +0300

    delete file

--- server/db/mongoose.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://127.0.0.1:27017/uplift-farmers', {
-  //connecting to the database
-  useNewUrlParser: true, // used to read data from the database
-  useCreatedIndex: true //use to create indices
-});
